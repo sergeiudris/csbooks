e046e30f3702f63bd3bee9510d1e4ef5a9689d10
sergeiudris <sergeiudris@gmail.com>
2019-03-16 09:03:44 +0300

    is-valid-storage

--- professional-clojure/app/src/ch3shortener/storage.clj
+++ professional-clojure/app/src/ch3shortener/storage.clj
@@ -3,6 +3,26 @@
             )
   )
 
+; create a link with the supplied ID and URL
+; retrieve a link, given its ID
+; update a link, given its ID and URL
+; DELETE a link, given its ID
+; list all known links
+
 (defprotocol Storage
-  
+  (create-link [this id url]
+    "Store the url under the id. Returns the id if successful, nil if the id is already in use"
+    )
+  (get-link [this id]
+    "given an ID, returns the associtaed URL. Returns nil if there is no associtaed URL"
+    )
+  (update-link [this id new-url]
+    "updates id to point to new-url. returns the id if successful, nil if the id has not yet been created"
+    )
+  (delete-link [this id]
+    "removes a link with the given ID from storage, if exists"
+    )
+  (list-links [this]
+    "returns a map of all known Ids to URLS"
+    )
   )
