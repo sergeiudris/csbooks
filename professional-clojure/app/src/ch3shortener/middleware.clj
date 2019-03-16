1adb2489f2cad555649e80617b3feabcd2f91606
sergeiudris <sergeiudris@gmail.com>
2019-03-16 13:39:05 +0300

    testing wrap-slurp-body middleware

--- professional-clojure/app/src/ch3shortener/middleware.clj
+++ professional-clojure/app/src/ch3shortener/middleware.clj
@@ -1,5 +1,17 @@
+; (ns ch3shortener.middleware
+;   (:require [clojure.repl :refer :all]
+;             )
+;   (:import [java.io.InputStream] )
+;   )
 (ns ch3shortener.middleware
-  (:require [clojure.repl :refer :all]
-            )
-  )
+  (:import [java.io InputStream]))
+
+(defn wrap-slurp-body
+  "slurps BiteArrayInputStream into e.g. string"
+  [handler]
+  (fn [request]
+    (if (instance? java.io.InputStream (:body request))
+      (let [prepared-request (update request :body slurp)]
+        (handler prepared-request))
+      (handler request))))
 
--- professional-clojure/app/test/ch3shortener/middleware_test.clj
+++ professional-clojure/app/test/ch3shortener/middleware_test.clj
@@ -6,3 +6,46 @@
             )
   )
 
+(deftest wrap-slurp-body-test
+  (let [body "this is a body"
+        request (mock/request :post "/foo" body)
+        expected-request (assoc request :body body ) 
+        indentity-handler (wrap-slurp-body identity )
+        
+        ]
+    
+    (testing "when givena request with a ByteArrayInputStream body"
+      (let [prepared-request (indentity-handler request ) ]
+        
+        (testing "the body is turned into a string"
+          (is (= body (:body prepared-request)))
+          (testing "and the rest of the request is unchanged"
+            (is (= expected-request prepared-request))
+            )
+          )
+        )
+      )
+    
+    (testing "when given a request that has no body"
+      (let [no-body (mock/request :get "/" ) ]
+        (testing "there's no effect"
+          (is (= no-body (indentity-handler no-body ) ))
+          )
+        )
+      )
+    
+    (testing "applying the middleware a second time has no effect"
+      (let [request (mock/request :post "/foo"  body )]
+        (is (= expected-request 
+              (->
+               request
+               indentity-handler
+               indentity-handler
+               ) 
+               ))
+        
+        )
+      )
+    
+    )
+  )
