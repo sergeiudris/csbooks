c929db23b83dd5f1db20b0c5b1816cb982f03f19
sergeiudris <sergeiudris@gmail.com>
2019-03-17 09:27:03 +0300

    added postgres, adminer

--- professional-clojure/professional-clojure-testing/src/fincalc/db.clj
+++ professional-clojure/professional-clojure-testing/src/fincalc/db.clj
@@ -1,7 +1,7 @@
 (ns fincalc.db
   (:require [clojure.java.jdbc :as sql]))
 
-(def db-spec "postgresql://localhost:5432/fincalc")
+(def db-spec "postgresql://postgres:postgres@postgresdb:5432/fincalc")
 
 (defn get-symbols [db-spec]
   (map :symbol (sql/query db-spec ["select * from stocks"])))
@@ -14,3 +14,12 @@
 
 (defn remove-symbol [db-spec sym]
   (sql/delete! db-spec :stocks ["symbol = ?" sym]))
+
+
+(comment
+  
+  ; (def db db-spec)
+  
+  (sql/db-do-commands db-spec (sql/create-table-ddl :stocks [:symbol "varchar(10)"]))
+  
+  )
