163dc00703bad4b19ec6d32a1b758a2bbf5bce6f
sergeiudris <sergeiudris@gmail.com>
2019-03-17 13:10:18 +0300

    added svg circle

--- professional-clojure/whip/src/whip/core.cljs
+++ professional-clojure/whip/src/whip/core.cljs
@@ -1,9 +1,9 @@
-(ns whip.core
+(ns ^:figwheel-always whip.core
     (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/whip/core.cljs. Go ahead and edit it and see reloading in action.")
+(println "This text is printed from src/whip/core.cljs. Go ahead and edit it and see reloading in action.!")
 
 ;; define your app data so that it doesn't get over-written on reload
 
@@ -13,7 +13,23 @@
 (defn hello-world []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h3 "Edit this and watch it change!!!"]
+   [:h4 {:id "stories"
+         :class "stories main"
+         :style  {:font-family "cursive"}}
+    "whip project management tool"
+    ]
+   [:h4#stories.main
+    "- - -"
+    ]
+   [:svg  
+   [:circle {:r 20
+             :cx 40
+             :cy 40
+             :fill "green"
+             }] 
+    ]
+   ])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
@@ -23,3 +39,10 @@
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
+
+
+(comment
+  
+  (+ 1 1)
+  
+  )
