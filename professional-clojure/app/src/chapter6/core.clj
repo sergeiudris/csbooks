e646a27b0eb0425b0df706dfc37d0a64721b2d94
sergeiudris <sergeiudris@gmail.com>
2019-03-18 09:20:21 +0300

    wow, amazingle datomic uses lazy EntityMap that can be resolved using clojure itself; even get-in to get reference props; no words

--- professional-clojure/app/src/chapter6/core.clj
+++ professional-clojure/app/src/chapter6/core.clj
@@ -3,7 +3,7 @@
             [clojure.pprint :as pp]
             [clojure.java.io :as io]
             [clojure.repl :refer :all]
-            [setup]
+            [setup :refer [db]]
             )
   (:import datomic.Util)
   )
@@ -15,6 +15,39 @@
 (comment
 
   (setup/go)
+  
+  (d/basis-t (db))
+  
+  (d/t->tx (d/basis-t (db)))
+  
+  (first (d/datoms (db) :aevt  ))
+  
+  (first (d/datoms (db) :aevt :db/doc :task/title ))
+  
+  (def entid (d/entid (db) :task/title))
+  
+  (d/ident (db) entid)
+  
+  
+  
+  (def jane (d/entity (db) [:user/login "janed"]))
+  
+  (keys jane)
+  
+  (:user/login jane)
+  
+  (vals jane)
+  
+  (type jane)
+  
+  (into {} jane)
+  
+  (get-in jane [:user/account :account/type])
+  
+  
+  
+  
+  
 
   
   )
