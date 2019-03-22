d5063b4ea0183eeb8dc23e0e2694c920a7c1f385
sergeiudris <sergei.udris@gmail.com>
2019-03-22 16:34:15 +0300

    removing code chaos

--- structure-and-interpretation-of-computer-programs2/app/src/sicp/exercises_1.clj
+++ structure-and-interpretation-of-computer-programs2/app/src/sicp/exercises_1.clj
@@ -1,49 +1,49 @@
 (ns sicp.exercises-1
   (:require [clojure.repl :refer :all]
-            [sicp.ch1-building-abstractions-with-procedures :refer [square average improve sqrt good-enough?  abs]]
-            )
-  )
+            [sicp.ch1-building-abstractions-with-procedures :refer [square average improve sqrt good-enough?  abs]]))
+            
+  
 
 
   
   ;; Exercise 1.3
   
   
-  (def sum-squares-of-two-max-numbers 
-    (fn [x y z]
-      (cond
-        (and (> x y) (> y z) ) (+ (square x) (square y)) 
-        ( and (< x y) (< y z) ) (+ (square y) (square z) )
-        :else (+ (square x) (square z))
-        )
+(def sum-squares-of-two-max-numbers 
+  (fn [x y z]
+    (cond
+      (and (> x y) (> y z) ) (+ (square x) (square y)) 
+      ( and (< x y) (< y z) ) (+ (square y) (square z))
+      :else (+ (square x) (square z)))))
+        
       
-      )
-    )
+      
+    
   
-  (sum-squares-of-two-max-numbers 1 2 3 )
+(sum-squares-of-two-max-numbers 1 2 3)
   
-  (sum-squares-of-two-max-numbers 1 1 3)
+(sum-squares-of-two-max-numbers 1 1 3)
   
-  (sum-squares-of-two-max-numbers -2 1 3)
+(sum-squares-of-two-max-numbers -2 1 3)
   
-  (sum-squares-of-two-max-numbers 4 1 2)
+(sum-squares-of-two-max-numbers 4 1 2)
   
   
   ;; Exercise 1.4
   
-  (def a 4)
-  (def b -3)
+(def a 4)
+(def b -3)
   
-  (- b 1)
+(- b 1)
   
-  (- a b)
+(- a b)
   
   ;; Exercise 1.5 Ben Bitdiddle
   
-  (def p (fn [] (p) ) )
-  (def test-xy (fn [x y]
-                 (if (= x 0) 0 y )
-                 ))
+(def p (fn [] (p)))
+(def test-xy (fn [x y]
+               (if (= x 0) 0 y)))
+                 
   
   ; (test-xy 0 (p) ) will be infinite loop
   
@@ -52,29 +52,29 @@
   
   ;; Exercise 1.6 
   
-  (defn new-if 
-    [predicate then-clause else-clause ]
-    (cond
-      predicate then-clause
-      :else else-clause
-      )
-    )
+(defn new-if 
+  [predicate then-clause else-clause]
+  (cond
+    predicate then-clause
+    :else else-clause))
+      
+    
   
-  (new-if (= 2 3) 0 5 )
-  (new-if (= 1 1) 0 5)
+(new-if (= 2 3) 0 5)
+(new-if (= 1 1) 0 5)
   
-  (defn sqrt-iter
-    [guess x]
-    (new-if (good-enough? guess x) 
-            guess
-            (sqrt-iter (improve guess x ) x ) 
-            )
-    )
+(defn sqrt-iter
+  [guess x]
+  (new-if (good-enough? guess x) 
+          guess
+          (sqrt-iter (improve guess x) x))) 
+            
+    
 
-  (defn sqroot
-    [x]
-    (sqrt-iter 1.0 x) 
-    )
+(defn sqroot
+  [x]
+  (sqrt-iter 1.0 x)) 
+    
 
 ; (sqroot 2) ; will fail, as new-if will evaluate second argument, will be StackOverflow
 
@@ -106,16 +106,16 @@
   [guess old-guess  x]
   (if (ok-enough? guess old-guess)
     guess
-    (sqrt-iter2 (improve guess x) guess x)
+    (sqrt-iter2 (improve guess x) guess x)))
     ;  (recur (improve guess x) guess x)
-    ))
+    
 
 
-  (defn sqroot2
-    [x]
-    (sqrt-iter2 1.0 0.1 x))
+(defn sqroot2
+  [x]
+  (sqrt-iter2 1.0 0.1 x))
 
-(ok-enough? 1.0 0.9 )
+(ok-enough? 1.0 0.9)
 
 (Math/sqrt 2) ; 1.4142135623730951
 (sqrt 2)      ; 1.4142156862745097
@@ -140,25 +140,22 @@
 (defn improve-cbrt
   [x guess]
   (/ 
-    (+ (/ x (square guess) ) (* guess 2) ) 
-    3 
-    )
-  )
+    (+ (/ x (square guess)) (* guess 2)) 
+    3)) 
+    
+  
 
 (improve-cbrt 2 1.5)
 
 (defn cbrt-iter
-     [ guess old-guess x]
-     (if (ok-enough? guess old-guess ) 
-       guess
-       (cbrt-iter (improve-cbrt x guess ) guess x )
-       )
-     
-     )
+  [ guess old-guess x]
+  (if (ok-enough? guess old-guess) 
+    guess
+    (cbrt-iter (improve-cbrt x guess) guess x)))
 
 (defn cbrt [x]
-  (cbrt-iter 1.0 0.1 x )
-  )
+  (cbrt-iter 1.0 0.1 x))
+  
 
 
 (Math/cbrt 2)
@@ -168,7 +165,6 @@
 
 
 
-  
   
   
   
