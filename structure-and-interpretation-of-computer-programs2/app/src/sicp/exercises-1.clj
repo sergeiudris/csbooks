32d720b876edfd854e6902a24d795672435e7b9a
sergeiudris <sergei.udris@gmail.com>
2019-03-22 14:01:43 +0300

    ben bitdiddle

--- structure-and-interpretation-of-computer-programs2/app/src/sicp/exercises-1.clj
+++ structure-and-interpretation-of-computer-programs2/app/src/sicp/exercises-1.clj
@@ -30,6 +30,26 @@
   (sum-squares-of-two-max-numbers 4 1 2)
   
   
+  ;; Exercise 1.4
+  
+  (def a 4)
+  (def b -3)
+  
+  (- b 1)
+  
+  (- a b)
+  
+  ;; Exercise 1.5 Ben Bitdiddle
+  
+  (def p (fn [] (p) ) )
+  (def test-xy (fn [x y]
+                 (if (= x 0) 0 y )
+                 ))
+  
+  (test-xy 0 (p) )
+  
+  ; applicative - inifine loop
+  ; normal - 0
   
   
   
