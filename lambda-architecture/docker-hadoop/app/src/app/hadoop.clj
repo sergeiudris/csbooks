07af0e182cd61681f4c080df954ca0b93c2896dc
sergeiudris <sergei.udris@gmail.com>
2019-04-06 15:24:36 +0300

    pail logins read/write using java + clj

--- lambda-architecture/docker-hadoop/app/src/app/hadoop.clj
+++ lambda-architecture/docker-hadoop/app/src/app/hadoop.clj
@@ -6,7 +6,10 @@
             ; [org.apache.hadoop/hadoop-core]
    ;
    )
-  (:import [com.backtype.hadoop.pail Pail])
+  (:import 
+   (com.backtype.hadoop.pail Pail PailStructure)
+   (app.java Login LoginPailStructure)
+   )
   )
 
 
@@ -14,14 +17,14 @@
   (doc doc)
   (use 'cascalog.api)
   (use 'cascalog.playground)
-  
-  (def pail (Pail/create "/tmp/mypail") )
-  (def os (.openWrite pail) )
-  (.writeObject os (bytes (byte-array [1 2 3] )) )
+
+  (def pail (Pail/create "/tmp/mypail"))
+  (def os (.openWrite pail))
+  (.writeObject os (bytes (byte-array [1 2 3])))
   (.writeObject os (bytes (byte-array [1 2 3 4])))
   (.writeObject os (bytes (byte-array [1 2 3 4 5])))
   (.close os)
-  
+
   (defn simpleIO
     []
     (let
@@ -31,24 +34,59 @@
       (.writeObject os (bytes (byte-array [1 2 3 4])))
       (.writeObject os (bytes (byte-array [1 2 3 4 5])))
       (.close os)
-      os
-      ))
-  
-  (def os (simpleIO) )
-  
-  
-  
-  
+      os))
+
+  (def os (simpleIO))
+
+  ; (gen-class :name app.hadoop.Login
+  ;            :prefix login
+  ;            :main false)
+
+
+  ; (gen-class :name app.hadoop.LoginPailStructure 
+  ;            :extends  PailStructure
+  ;            :state :state
+  ;            :prefix login-pail-structure
+  ;            :main false
+
+  ;            )
+
+
 
   (.getName Pail)
   ; (.create Pail)
-  
+
   (doc bytes)
   (doc byte-array)
+
+
+  (bytes (byte-array [1 2 3]))
+
+
+  (defn write-logins
+    []
+    (let [
+          login-pail (Pail/create "/tmp/mylogins" (LoginPailStructure.) )
+          out (.openWrite login-pail)
+          ]
+      (.writeObject out (Login. "alex" 123456789231 ) )
+      (.writeObject out (Login. "bob"  135123423413))
+      (.close out)))
   
+  (write-logins)
   
-  (bytes (byte-array [1 2 3] ))
+  (defn read-logins
+    []
+    (let [
+          login-pail (Pail. "/tmp/mylogins")
+          ]
+      (for [l login-pail]
+        (prn (.-userName l) (.-loginUnixTime l) ) 
+        )
+      ))
   
-  sentence
+  (read-logins)
+
+
   ;
   )
