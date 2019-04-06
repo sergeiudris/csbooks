d04c2ee99751a92e2c7fde02a7b60c8472a1628f
sergeiudris <sergei.udris@gmail.com>
2019-04-06 12:23:05 +0300

    creating pails locally

--- lambda-architecture/docker-hadoop/app/src/app/hadoop.clj
+++ lambda-architecture/docker-hadoop/app/src/app/hadoop.clj
@@ -2,17 +2,53 @@
   (:require [clojure.repl :refer :all]
             [cascalog.api :as cs]
             ; [cascalog.playground :as csp]
-            ; [com.backtype/dfs-datastores]
+            ; [com.backtype.hadoop.pail]
             ; [org.apache.hadoop/hadoop-core]
    ;
-   ))
+   )
+  (:import [com.backtype.hadoop.pail Pail])
+  )
 
 
 (comment
   (doc doc)
   (use 'cascalog.api)
   (use 'cascalog.playground)
+  
+  (def pail (Pail/create "/tmp/mypail") )
+  (def os (.openWrite pail) )
+  (.writeObject os (bytes (byte-array [1 2 3] )) )
+  (.writeObject os (bytes (byte-array [1 2 3 4])))
+  (.writeObject os (bytes (byte-array [1 2 3 4 5])))
+  (.close os)
+  
+  (defn simpleIO
+    []
+    (let
+     [pail (Pail/create "/tmp/mypail2")
+      os   (.openWrite pail)]
+      (.writeObject os (bytes (byte-array [1 2 3])))
+      (.writeObject os (bytes (byte-array [1 2 3 4])))
+      (.writeObject os (bytes (byte-array [1 2 3 4 5])))
+      (.close os)
+      os
+      ))
+  
+  (def os (simpleIO) )
+  
+  
+  
+  
 
+  (.getName Pail)
+  ; (.create Pail)
+  
+  (doc bytes)
+  (doc byte-array)
+  
+  
+  (bytes (byte-array [1 2 3] ))
+  
   sentence
   ;
   )
