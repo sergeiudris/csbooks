6ddbe60c68309aa2424874925545277b030b7709
sergeiudris <sergei.udris@gmail.com>
2019-04-07 11:55:37 +0300

    compiled classes, not sure of work

--- lambda-architecture/docker-hadoop/app/project.clj
+++ lambda-architecture/docker-hadoop/app/project.clj
@@ -3,6 +3,9 @@
                                     ; :creds :gpg
                                    }
                  "conjars" "https://conjars.org/repo"
+                 "clojars" "https://clojars.org/repo"
+                 "oracle"  "https://download.oracle.com/maven"
+                 
                  }
             
   :min-lein-version "2.0.0"
@@ -46,13 +49,28 @@
 
 
                  [com.backtype/dfs-datastores "1.3.6"]
-                 [cascalog/cascalog-core "3.0.0"]
-                 [backtype/cascading-thrift "0.2.3" :exclusions [org.apache.thrift/libthrift]]
+                 [backtype/dfs-datastores-cascading "1.2.0"]
+                ;  [cascalog/cascalog-core "3.0.0"]
+                 [cascalog/cascalog "1.10.0"]
+                ;  [backtype/cascading-thrift "0.2.3" :exclusions [org.apache.thrift/libthrift]]
                  [javax.annotation/javax.annotation-api "1.3.2"]
-                ;  [org.apache.thrift/libthrift "0.6.1"]
+                 [org.apache.thrift/libthrift "0.9.3"]
+                 [org.apache.storm/storm-core "0.9.4"]
+                 [com.clearspring.analytics/stream "2.7.0"]
+                 [org.apache.zookeeper/zookeeper "3.4.6"]
+                 [org.hectorclient/hector-core "2.0-0"]
+                 [elephantdb/elephantdb-cascalog "0.4.5"]
+                 [org.apache.kafka/kafka_2.9.2 "0.8.1.1"]
+                 [org.apache.storm/storm-kafka "0.9.4"]
+                 [elephantdb/elephantdb-bdb "0.4.5"]
+                 
+                 
                  [org.apache.hadoop/hadoop-client "2.7.1"]
                  [org.apache.hadoop/hadoop-hdfs "2.7.1"]
                  [commons-io/commons-io "2.5"]
+                 [commons-fileupload/commons-fileupload "1.3"]
+                ;  [org.apache.commons/commons-fileupload "1.4"]
+                ;  [org.apache.commons/commons-io "1.3.2"]
                  [org.apache.hadoop/hadoop-common "2.7.1"]
                  
                  ]
