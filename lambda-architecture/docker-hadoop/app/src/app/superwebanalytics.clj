c25e98eff1475e418ba3268f7715d6d2ded59b5f
sergeiudris <sergei.udris@gmail.com>
2019-04-07 08:39:18 +0300

    ThriftPailStructure

---
+++ lambda-architecture/docker-hadoop/app/src/app/superwebanalytics.clj
@@ -0,0 +1,29 @@
+(ns app.superwebanalytics  
+  (:require [clojure.repl :refer :all]
+            [cascalog.api :as cs]
+   [clojure.reflect :as r]
+   [clojure.pprint :refer [print-table] :as pp]
+   ;
+   )
+  (:import 
+   (com.backtype.hadoop.pail Pail PailStructure SequenceFileFormat PailSpec)
+   (app.java Login LoginPailStructure PartitionedLoginPailStructure)
+   (app.java.tap ThriftPailStructure)
+   
+   (org.apache.hadoop.conf Configuration)
+   
+   (org.apache.hadoop.hdfs DistributedFileSystem)
+   (java.net URI)
+   (java.util HashMap)
+   
+   (org.apache.hadoop.fs FSDataInputStream FSDataOutputStream FileSystem Path LocalFileSystem)
+  ;  (org.apache.hadoop.io SequenceFile)
+   
+   )
+  )
+
+(comment
+ 
+  
+  ;
+  )
