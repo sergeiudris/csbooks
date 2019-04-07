3692f31d6c6faa8ba2b42f4b699ddb9f4e914f19
sergeiudris <sergei.udris@gmail.com>
2019-04-07 12:03:24 +0300

    DataPailStructure

--- lambda-architecture/docker-hadoop/app/src/manning/tap/DataPailStructure.java
+++ lambda-architecture/docker-hadoop/app/src/manning/tap/DataPailStructure.java
@@ -1,11 +1,22 @@
 package manning.tap;
 
 import manning.schema.Data;
+import java.util.Collections;
+import java.util.List;
+
 
 public class DataPailStructure extends ThriftPailStructure<Data> {
   @Override
   protected Data createThriftObject() {
     return new Data();
+  }
+
+  public List<String> getTarget(Data object) {
+    return Collections.EMPTY_LIST;
+  }
+
+  public boolean isValidTarget(String... dirs) {
+    return true;
   }
 
   public Class getType() {
