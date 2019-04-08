2e92f3aeeb86b446caa7474c8c383bc5f58eafec
sergeiudris <sergei.udris@gmail.com>
2019-04-08 15:45:31 +0300

    app w/ Pail

--- lambda-architecture/apps/app/src/app/hadoop.clj
+++ lambda-architecture/apps/app/src/app/hadoop.clj
@@ -1,26 +1,12 @@
 (ns app.hadoop  
   (:require [clojure.repl :refer :all]
-            [cascalog.api :as cs]
-   [clojure.reflect :as r]
-   [clojure.pprint :refer [print-table] :as pp]
-   
-            ; [cascalog.playground :as csp]
-            ; [com.backtype.hadoop.pail]
-            ; [org.apache.hadoop/hadoop-core]
-   ;
-   )
+            [clojure.reflect :as r]
+            [clojure.pprint :refer [print-table] :as pp]
+            )
   (:import 
    (com.backtype.hadoop.pail Pail PailStructure SequenceFileFormat PailSpec)
    (app.java Login LoginPailStructure PartitionedLoginPailStructure)
-   (org.apache.hadoop.conf Configuration)
-  (org.apache.commons.io IOUtils)
-   (org.apache.hadoop.hdfs DistributedFileSystem)
-   (java.net URI)
    (java.util HashMap)
-   
-   (org.apache.hadoop.fs FSDataInputStream FSDataOutputStream FileSystem Path LocalFileSystem)
-  ;  (org.apache.hadoop.io IOUtils)
-   
    )
   )
 
@@ -131,65 +117,6 @@
 
   (map #(prn %1) {1 2 2 3}  )
   
-  (def hdfsuri "hdfs://namenode:8020")
-  (def hdfsuri-file01 "hdfs://namenode:8020/user/joe/wordcount/input/file01")
-  (def hdfsuri-file03 "hdfs://namenode:8020/user/joe/wordcount/input/file03")
-  
-  ; hadoop fs -cat hdfs://localhost:8020/user/joe/wordcount/input/file01
-  (defn hadoop-conf
-    []
-      (let [conf (Configuration.) ]
-            (.set conf "fs.defaultFS" hdfsuri)
-            (.set conf "fs.hdfs.impl" (.getName org.apache.hadoop.hdfs.DistributedFileSystem))
-            (.set conf "fs.fs.impl" (.getName org.apache.hadoop.fs.LocalFileSystem))
-            (System/setProperty "HADOOP_USER_NAME" "hdfs")
-            (System/setProperty "hadoop.home.dir" "/")
-        conf
-        ))
-
-  (defn hdfs-fs
-    []
-    (let [
-          conf (hadoop-conf)
-          fs (FileSystem/get  (URI/create hdfsuri) conf )
-          ]
-      fs
-      )
-    )
-  
-  (def fs (hdfs-fs) )
-  
-  (.getWorkingDirectory fs)
-  
-  ; https://mvnrepository.com/artifact/org.apache.hadoop
-  ; https://creativedata.atlassian.net/wiki/spaces/SAP/pages/52199514/Java+-+Read+Write+files+with+HDFS
-  (defn read-file
-    [path]
-    (let [
-          hdfs-path (Path. path)
-          is (.open fs hdfs-path)
-          out (. IOUtils toString is "UTF-8")
-          ]
-      (prn out)
-      (.close is)
-      )
-    )
-  
-  (read-file hdfsuri-file01)
-  
-  (defn write-file
-    [path content]
-     (let [hdfs-path (Path. path)
-           os        (.create fs hdfs-path)
-           ]
-       (.writeBytes os content)
-       (.close os)
-       )
-    )
-  
-  (write-file hdfsuri-file03 "abc2")
-  
-  (read-file hdfsuri-file03)
   
   ; https://livebook.manning.com/#!/book/big-data/about-this-book/2
   (defn partition-data
