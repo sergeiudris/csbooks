4b5ffa67cfa41f7ebc22ca3e4b9a0f6d7592ab5c
sergeiudris <sergei.udris@gmail.com>
2019-04-08 16:46:01 +0300

    can i connect to hdfs:// using cascalog ?

--- lambda-architecture/apps/app_cascalog/src/app/cascalog.clj
+++ lambda-architecture/apps/app_cascalog/src/app/cascalog.clj
@@ -1,7 +1,9 @@
 (ns app.cascalog
   (:require [clojure.repl :refer :all]
-            [cascalog.api :as cs]
-            [cascalog.playground :as csp]
+            [cascalog.api :refer :all]
+            [cascalog.logic.ops :as c]
+            [cascalog.logic.def :as defc]
+            [cascalog.playground :refer :all]
             [clojure.pprint :refer [print-table] :as pp]
    ;
             )
@@ -9,6 +11,26 @@
 
 (comment
   (clojure-version)
-  csp/sentence
   ;
+  )
+
+
+(comment
+  (?- (stdout)
+      sentence)
+  
+  (?- (stdout)
+      (<- [?line]
+          (sentence :> ?line)))
+  
+  (defc/defmapcatfn tokenise [line]
+    "reads in a line of string and splits it by a regular expression"
+    (clojure.string/split line #"[\[\]\\\(\),.)\s]+"))
+  
+  
+  (?<- (stdout)
+       [?word ?count]
+       (sentence ?line)
+       (tokenise ?line :> ?word)
+       (c/count ?count))
   )
