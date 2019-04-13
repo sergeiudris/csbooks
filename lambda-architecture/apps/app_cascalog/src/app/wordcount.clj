2e21f359b472370c5707069375814f5aaf1b191b
sergeiudris <sergei.udris@gmail.com>
2019-04-13 15:59:08 +0300

    wordcount.clj

--- lambda-architecture/apps/app_cascalog/src/app/wordcount.clj
+++ lambda-architecture/apps/app_cascalog/src/app/wordcount.clj
@@ -26,7 +26,7 @@
     []
     (let [uri-base "hdfs://namenode:9820"
           uri-in (str uri-base "/user/joe/wordcount/input" )
-          uri-out (str uri-base "/user/joe/wordcount/output8")
+          uri-out (str uri-base "/user/joe/wordcount/output100")
           ]
       (WordCount/main (into-array [uri-in uri-out]))
       )
