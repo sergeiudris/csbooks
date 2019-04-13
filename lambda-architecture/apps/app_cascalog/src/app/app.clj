4f1563df2cbd3eb838b511628de2932a52906332
sergeiudris <sergei.udris@gmail.com>
2019-04-13 16:23:57 +0300

    uberjar into hadoop; error w/ deps conflict

--- lambda-architecture/apps/app_cascalog/src/app/app.clj
+++ lambda-architecture/apps/app_cascalog/src/app/app.clj
@@ -1,12 +1,19 @@
 (ns app.app
+  (:gen-class)
   (:require [shared.nrepl]
             [shared.core]
             [app.cascalog]
             [app.jcascalog]
             [app.wordcount]
-            [shared.pedestal.server]))
+            [shared.pedestal.server])
+  (:import 
+   (app.java WordCount)
+   )
+  )
 
-(defn -main []
+(defn -main  [& args]
+  ; (WordCount/main (into-array ["/user/joe/wordcount/input" "/user/joe/wordcount/output101"]))
   (shared.nrepl/-main)
-  (shared.pedestal.server/run-dev))
+  (shared.pedestal.server/run-dev)
+  )
 
