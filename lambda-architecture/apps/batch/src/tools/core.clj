75dcd81eae2e305f250795cb6b01613b346d1fa4
sergeiudris <sergei.udris@gmail.com>
2019-04-14 13:00:34 +0300

    batch app structure for one-repl-many-apps hadoop fast development

---
+++ lambda-architecture/apps/batch/src/tools/core.clj
@@ -0,0 +1,18 @@
+(ns shared.core
+  (:require [clojure.repl :refer :all]
+            ;
+            ))
+
+
+(defn parse-int [number-string]
+  (try (Integer/parseInt number-string)
+       (catch Exception e nil)))
+
+
+(comment
+
+
+  (parse-int "3")
+
+  ;
+  )
