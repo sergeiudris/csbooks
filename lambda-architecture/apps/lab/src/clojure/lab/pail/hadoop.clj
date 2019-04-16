8c81ab9f155b0240eda666b1cd2b373d1df7f046
sergeiudris <sergei.udris@gmail.com>
2019-04-16 06:37:10 +0300

    vscode settings.json

--- lambda-architecture/apps/lab/src/clojure/lab/pail/hadoop.clj
+++ lambda-architecture/apps/lab/src/clojure/lab/pail/hadoop.clj
@@ -152,7 +152,7 @@
   
   
   
-  (partition-data "/tmp/partitaioned_lo41gai5ns6waaa1244a3wa1")
+  (partition-data "/tmp/partitaioned_lo41gai5ns6waaa1244a31wa1")
   
   (read-logins  "/tmp/partitioned_logins6")
   
