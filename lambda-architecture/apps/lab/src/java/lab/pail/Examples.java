b74062e3266b0580d546d73bffb5f8ffef07d33b
sergeiudris <sergei.udris@gmail.com>
2019-04-17 08:36:18 +0300

    java also does not work

---
+++ lambda-architecture/apps/lab/src/java/lab/pail/Examples.java
@@ -0,0 +1,16 @@
+package lab.pail;
+
+import java.io.IOException;
+import com.backtype.hadoop.pail.Pail;
+import com.backtype.hadoop.pail.Pail.TypedRecordOutputStream;
+
+public class Examples {
+  public static void partitionData(String path) throws IOException {
+    Pail<Login> pail = Pail.create(path, new PartitionedLoginPailStructure());
+
+    TypedRecordOutputStream os = pail.openWrite();
+    os.writeObject(new Login("chris", 1352702020));
+    os.writeObject(new Login("david", 1352788472));
+    os.close();
+  }
+}
