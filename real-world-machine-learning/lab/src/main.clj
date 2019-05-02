da60c5e9b89592a06080d26e33f4664bdb562afe
sergeiudris <sergei.udris@gmail.com>
2019-05-02 08:21:17 +0300

    real world machine learninig

---
+++ real-world-machine-learning/lab/src/main.clj
@@ -0,0 +1,37 @@
+(ns main
+  (:require [tools.nrepl]
+            [tools.core]
+            [tools.pedestal.server]
+
+   ;
+            )
+
+  ; (:import (jva.casc Examples ))
+  
+  ;
+  )
+
+
+(defn -dev  [& args]
+  (tools.pedestal.server/run-dev)
+  (tools.nrepl/-main)
+  )
+
+(defn -main  [& args]
+  (tools.nrepl/-main)
+  (tools.pedestal.server/run-dev)
+  )
+
+(comment
+  
+  
+  ; (Examples/hello)
+  -dev
+
+  (System/getProperty "java.vm.version")
+  (System/getProperty "java.version")
+  (System/getProperty "java.specification.version")
+  (clojure-version)
+
+  ;
+  )
