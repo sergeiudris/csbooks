adcdd7d8b5f0559bb31a41809628efcd4f9e2645
sergeiudris <sergei.udris@gmail.com>
2019-05-18 16:57:53 +0300

    reduced time of k-means from 186s to 34s using two pmaps

---
+++ applied-la/lab/src/ala/kmeans.clj
@@ -0,0 +1,253 @@
+(ns ala.kmeans
+  (:require [clojure.repl :refer :all]
+            [ala.print :refer [cprn]]
+            [ala.math :refer :all])
+  ;
+  )
+
+
+(defn j-clust
+  "Returns a scalar J (clustering objective estimate).
+   The lower the better clustering is.
+   Mean square distance from vecs to their representatives.
+   "
+  [xs zs cs]
+  (->
+   (reduce-kv (fn [acc i x]
+                (+ acc (sq (vec-dist x (zs (cs i)))))) 0 xs)
+   (/ (count xs))))
+
+
+
+(defn j-clust-nearest
+  "Returns a scalar J.
+   Is a version of j-lcust, picks min||x - z|| (distance from vec to representative)
+   to minimize J.
+  "
+  [xs zs]
+  (->
+   (reduce-kv (fn [acc i x]
+                (+ acc (sq (vec-dist x (second (nearest-neighbor x zs)))))) 0 xs)
+   (/ (count xs))))
+
+(defn j-clust-nearest-p
+  "Returns a scalar J.
+   Is a version of j-lcust, picks min||x - z|| (distance from vec to representative)
+   to minimize J.
+  "
+  [xs zs]
+  (as-> nil R
+    (pmap (fn [x]
+            [x (sq (vec-dist x (second (nearest-neighbor x zs))))]) xs)
+    (vec R)
+    (reduce-kv (fn [acc i [x sq-dist]]
+                 (+ acc sq-dist)) 0 R)
+    (/ R (count xs))
+    ;
+    ))
+
+(defn group-centroid
+  "Returns the average of vecs"
+  [xs]
+  (apply vecs-mean xs))
+
+
+(defn -k-means-lazy-partition
+  "Does not work, take-while is sequential, coll is not sorted"
+  [xs zs]
+  (lazy-seq
+   (when-let [xs-s (seq xs)]
+     (let [fst       (first xs-s)
+           nrst-neig (second (nearest-neighbor fst zs))
+           run       (cons fst (take-while #(= nrst-neig (second (nearest-neighbor % zs))) (next xs-s)))]
+       (cprn run)
+       (prn)
+       (cons run (-k-means-lazy-partition (lazy-seq (drop (count run) xs-s))  zs))
+        ;
+       ))))
+
+(defn k-means-iteration
+  "Returns k-means groups after one iteration."
+  [xs zs]
+  (persistent!
+   (reduce-kv
+    (fn [acc i x]
+      (let [nrst-neig (nearest-neighbor x zs)
+            i-zs      (first nrst-neig)]
+        (assoc! acc i-zs (conj (get acc i-zs []) x)))
+      ;
+      )
+    (transient {}) xs)))
+
+(defn k-means-iteration-p
+  "Returns k-means groups after one iteration."
+  [xs zs]
+  (as-> nil R
+    (pmap (fn [x]
+            [x (nearest-neighbor x zs)])
+          xs)
+    (vec R)
+    (persistent!
+     (reduce-kv
+      (fn [acc i [x nrst-neig]]
+        (let [i-zs      (first nrst-neig)]
+          (assoc! acc i-zs (conj (get acc i-zs []) x)))
+      ;
+        )
+      (transient {}) R))
+    ;
+    ))
+
+(defn k-means
+  "Returns a group map {i-group [[x] [x] ..] .. }.
+   Iterates until J (objective) is minimized"
+  ([xs zs]
+   (k-means xs zs nil 0 nil nil))
+  ([xs zs J-prev cnt iters ]
+  ;  (prn "zs:" (count zs))
+   (let    [groups (k-means-iteration-p xs zs)
+            J      (reduce + (mapv (fn [[i g]]
+                                     (j-clust-nearest-p g zs)) groups))]
+    ;  (cprn {:groups groups
+    ;         :iter   cnt
+    ;         :zs     zs
+    ;         :J      J})
+    ;  (prn "groups:" (keys groups))
+     (cond
+       (and (>= (inc cnt) (or iters 3)) (==* J J-prev)) {:J      J
+                                                 :zs     zs
+                                                 :iter   (inc cnt)
+                                                 :groups groups}
+       :else (k-means xs 
+                      (mapv (fn [[i g]] (apply vecs-mean g)) groups) 
+                      J (inc cnt) iters)
+       ;
+       ))))
+
+(defn k-means-rand-x->z
+  "Returns (k-means ..), representatives are chosen randomly from xs"
+  [xs & {:keys [k i]
+         :or   {k 3 i 3}}]
+  (prn ":k" k ":i" i)
+  (as-> nil zs
+    (mapv (fn [_] (rand-nth xs)) (range k))
+    (k-means xs zs nil 0 i)))
+
+(defn k-means-rand-z
+  "Returns (k-means ..), representatives are vecs of radndom ints"
+  [xs & {:keys [k i]
+         :or   {k 4 i 3}}]
+  (prn ":k" k ":i" i)
+  (as-> nil zs
+    (mapv (fn [_] (mapv (fn [_] (rand-int 255)) (range (count (first xs))))) (range k))
+    (k-means xs zs nil 0 i)))
+
+(defn partitionv
+  "Returns vec of vecs "
+  [ n v]
+  (->>
+   (partition n v)
+   (mapv vec)))
+
+(defn k-means-rand-gs
+  "Returns (k-means ..), representatives are means of initial random groups"
+  [xs & {:keys [k i]
+         :or   {k 3
+                i 3}}]
+  (prn ":k" k ":i" i)
+  (as-> nil R
+    (shuffle xs)
+    (partitionv (/ (count R) k) R)
+    (mapv (fn [g] (apply vecs-mean g)) R)
+    ; (cprn (count R) )
+    (k-means xs R nil 0 i)))
+
+(comment
+
+  (def points-A [[0.5 8.5]
+                 [2.2 11.5]
+                 [2.7 0]
+                 [3.7 2.5]
+                 [3.5 5.3]
+                 [3.5 7.2]
+                 [5 5.5]
+                 [7.1 5.1]
+                 [7.5 4.9]
+                 [7.8 6.8]])
+
+  (def points-B [[4.5 12.8]
+                 [6.1 13.8]
+                 [8 14.9]
+                 [8.5 14.6]
+                 [7.9 17.1]
+                 [12.5 18]
+                 [11 12.9]
+                 [11.5 10.1]
+                 [12.9 11]])
+  (def points-C [[10 7.5]
+                 [10.9 7.1]
+                 [12.1 10]
+                 [14.1 9.9]
+                 [12 4.5]
+                 [12.9 1]
+                 [15.8 0.2]
+                 [15.7 1.4]
+                 [15.9 7]
+                 [14.4 7.1]])
+
+  (def points (shuffle (concat points-A points-B points-C)))
+  (count points)
+
+  (def points-A-centroid [4 5])
+  (def points-B-centroid [9 15])
+  (def points-C-centroid [14 5])
+
+  (def zs [points-A-centroid points-B-centroid points-C-centroid])
+
+
+  (def clusters (vec (-k-means-lazy-partition points zs)))
+  (count clusters)
+
+  (nearest-neighbor [2.7 0] zs)
+  (nearest-neighbor [10 7.5] zs)
+
+  (partition-by odd? [1 2 2 3 1 4 5 6 8 10 9 1 2 7 4])
+
+  (def groups (k-means-iteration points zs))
+
+  (reduce + (mapv (fn [[i g]]
+                    (j-clust-nearest g zs)) groups))
+
+  (def zs-2 [[3 3] [8 8] [15 15]])
+
+  (def zs-3 [[0 10] [10 0] [13 15]])
+
+
+  (def clusters-2 (k-means points zs))
+
+  (def clusters-3 (k-means points zs-2)) ; successfully does 5 iterations, prn progress
+
+  (def clusters-4 (k-means points zs-3))
+
+  (def clusters-5 (k-means points (mapv (fn [_] (rand-nth points)) (range 3))))
+
+  (def clusters-6 (k-means points (mapv (fn [_] (rand-nth points)) (range 4))))
+
+  (def clusters-7 (k-means points (mapv (fn [_] (rand-nth points)) (range 5))))
+
+  (doseq [c [clusters-2 clusters-3 clusters-4 clusters-5]]
+    (cprn (c :J))
+    (cprn (c :zs)))
+
+  (doseq [c [clusters-5 clusters-6 clusters-7]]
+    (cprn (c :J))
+    (cprn (c :zs)))
+
+
+  (k-means-rand-x->z points :k 20 )
+
+
+  ;;;
+  )
+
+; heuristic
---
+++ applied-la/lab/src/ala/mnist.clj
@@ -0,0 +1,378 @@
+(ns ala.mnist
+  (:require [ala.math :refer :all]
+            [clojure.repl :refer :all]
+            [clojure.java.io :refer [file output-stream input-stream]]
+            [clojure.pprint  :as pp]
+            [clojure.java.io :as io]
+            [ala.kmeans :refer [k-means-rand-x->z k-means-rand-z k-means-rand-gs]]))
+
+; http://yann.lecun.com/exdb/mnist/
+
+(defn read-binary-file
+  "returns bute-array , accepts :offset :limit"
+  [filename & {:keys [offset limit]
+               :or   {offset 0}}]
+  (let [f   (file filename)
+        lim (or limit (- (.length f) offset))
+        buf (byte-array lim)]
+    ; (prn lim offset)
+    (with-open [in (input-stream  f)]
+      (.skip in offset)
+      (.read in buf 0 lim)
+      buf
+      ;
+      )))
+
+(defn read-file-to-vec
+  "returns vector with file bytes (ints 8)"
+  [filename offset]
+  (->
+   (if (.exists (io/as-file  filename))
+     (read-binary-file filename
+                       :offset  offset)
+     nil)
+   vec))
+
+(defn bytes->int [bytes]
+  "Converts a byte array into an integer."
+  (->>
+   bytes
+   (map (partial format "%02x"))
+   (apply (partial str "0x"))
+   read-string))
+
+(defn file-szie
+  "returns file byte length"
+  [filename]
+  (.length (file filename)))
+
+
+(def t10-iamges-filename "data/t10k-images-idx3-ubyte")
+(def t10-labels-filename "data/t10k-labels-idx1-ubyte")
+(def t60-iamges-filename "data/train-images-idx3-ubyte")
+(def t60-labels-filename "data/train-labels-idx1-ubyte")
+
+
+(def image-size 28)
+(def image-size-squared (* 28 28))
+
+
+(def image-count-t60 60000)
+(def image-count-t10 10000)
+
+(def t10-images-file-size 7840016)
+(def t60-images-file-size 47040016)
+
+(def t10-labels-file-size 10008)
+(def t60-labels-file-size 60008)
+
+(def image-file-meta-bits 16)
+(def labels-file-meta-bits 8)
+
+; reading files into memeory on init
+
+(def  T10-IMAGES  (read-file-to-vec t10-iamges-filename  image-file-meta-bits))
+(def  T60-IMAGES  (read-file-to-vec t60-iamges-filename  image-file-meta-bits))
+(def  T10-LABELS  (read-file-to-vec t10-labels-filename  labels-file-meta-bits))
+(def  T60-LABELS  (read-file-to-vec t60-labels-filename  labels-file-meta-bits))
+
+
+
+
+
+(comment
+
+  (->
+   (prn-mx (take 784 (drop 784 T10-IMAGES)) 28)
+   nil?)
+
+  ;;;
+  )
+
+(defn read-int-from-file
+  "returns the number represented by bytes in range [offset , + offset  limit]"
+  [filename offset limit]
+  (->   (read-binary-file  filename :offset offset :limit limit)
+        bytes->int)
+  )
+
+(defn read-MNIST-image
+  "returns vector of pixel values for an image at offset"
+  [filename offset]
+  (->>
+   (read-binary-file filename
+                     :offset (+ offset image-file-meta-bits)
+                     :limit (Math/pow image-size 2))
+   vec))
+
+(defn read-MNIST-label
+  "returns vector of pixel values for an image at offset"
+  [filename offset]
+  (->>
+   (read-binary-file filename
+                     :offset (+ offset labels-file-meta-bits)
+                     :limit 1)
+   vec))
+
+(defn read-MNIST-image-nth
+  "returns nth image from the file"
+  [filename  n]
+  (read-MNIST-image filename (* n (Math/pow image-size 2))))
+
+(defn read-MNIST-label-nth
+  "returns nth image from the file"
+  [filename  n]
+  (read-MNIST-label filename (* n 1)))
+
+(defn prn-nth-image
+  "prints nth image from file as matrix 28x28, using prn-mx "
+  [filename n]
+  (prn-mx (read-MNIST-image-nth filename n) image-size)
+  nil)
+
+(defn prn-nth-image-and-label-t10
+  "prn iamge and label from "
+  [n]
+  (prn-nth-image t10-iamges-filename n)
+  (read-MNIST-label-nth t10-labels-filename n))
+
+(defn prn-nth-image-and-label-t60
+  "prn iamge and label from "
+  [n]
+  (prn-nth-image t60-iamges-filename n)
+  (read-MNIST-label-nth t60-labels-filename n))
+
+(comment
+
+  (count (read-binary-file "data/t10k-images-idx3-ubyte"))
+
+  (as-> (read-binary-file "data/t10k-labels-idx1-ubyte") x
+    (bytes x)
+    (take 4 x))
+
+  (int (bytes (take 4 (read-binary-file "data/t10k-images-idx3-ubyte"))))
+
+  (subvec (read-binary-file "data/t10k-images-idx3-ubyte") 4 4)
+
+  (def x (byte-array [(byte 0x43)
+                      (byte 0x6c)
+                      (byte 0x6f)
+                      (byte 0x6a)
+                      (byte 0x75)
+                      (byte 0x72)
+                      (byte 0x65)
+                      (byte 0x21)]))
+
+  (String. x)
+
+  ; (clojure.main/repl :print pprint)
+
+  (pp/pprint x)
+
+  (apply str "0x" "00")
+
+  (read-string (format "%02x" (byte 0x00)))
+
+  (apply (partial str "0x") ["00" "02"])
+  (apply (partial str "0x") ["asd"])
+  (str "0x"  ["asd"])
+  
+  (file-szie "data/t10k-images-idx3-ubyte")
+  (file-szie "data/train-images-idx3-ubyte")
+  (file-szie "data/t10k-labels-idx1-ubyte")
+  
+  
+
+
+  ;;;
+  )
+
+
+(comment
+
+    ; number of iamges in t10
+  (read-int-from-file "data/t10k-images-idx3-ubyte" 4 4)
+
+    ; number of images rows
+  (read-int-from-file "data/t10k-images-idx3-ubyte" 8 4)
+
+    ; number of images rows
+  (read-int-from-file "data/t10k-images-idx3-ubyte" 12 4)
+
+    ; number of images in train
+  (read-int-from-file "data/train-images-idx3-ubyte" 4 4)
+
+
+  (count)
+
+  (->
+   (prn-mx (read-MNIST-image "data/t10k-images-idx3-ubyte" 0) 28)
+   nil?)
+
+  (->
+   (prn-mx (read-MNIST-image-nth "data/t10k-images-idx3-ubyte" 0) 28)
+   nil?)
+
+  (prn-nth-image "data/t10k-images-idx3-ubyte" 0)
+
+  (prn-nth-image "data/t10k-images-idx3-ubyte" 0)
+
+  (prn-nth-image "data/t10k-images-idx3-ubyte" 1555)
+
+  (read-MNIST-label "data/t10k-labels-idx1-ubyte" 0)
+
+
+  (prn-nth-image "data/t10k-images-idx3-ubyte" 1555)
+  (read-MNIST-label-nth "data/t10k-labels-idx1-ubyte" 1555)
+
+  (prn-nth-image "data/t10k-images-idx3-ubyte" 1000)
+  (read-MNIST-label-nth "data/t10k-labels-idx1-ubyte" 1000)
+
+  (prn-nth-image-and-label-t10 1000 )
+  
+  (prn-nth-image-and-label-t60 1000)
+  
+
+
+  ;;;
+  )
+
+
+
+(defn nth-image-0
+  "returns nth image from a dataset"
+  [dataset n]
+  (->>
+   dataset
+   (drop (* n image-size-squared))
+   (take image-size-squared)))
+
+(defn take-images-range
+  "reutrns vector with image data from image n-start to iamge n-end  "
+  [ n-start n-end dataset]
+  (let [i-start (* (dec n-start) image-size-squared)
+        i-end   (+ i-start (* (- (inc n-end) n-start) image-size-squared))]
+    (subvec dataset i-start i-end)))
+
+(defn nth-image
+  "returns nth image from a dataset"
+  [dataset i]
+  (subvec dataset  (* i image-size-squared) (+ (* i image-size-squared) image-size-squared)))
+
+(defn prn-image
+  "prints image as matrix 28x28 using prn-mx"
+  [image-vec]
+  (prn-mx  image-size image-vec)
+  nil)
+
+(defn prn-nth-image-dataset
+  "prints nth image from a dataset"
+  [dataset n]
+  (->
+   (nth-image dataset (dec n))
+   prn-image)
+  nil)
+
+(defn nth-label
+  "returns nth label from a dataset"
+  [dataset i]
+  (dataset i)
+  )
+
+(defn prn-nth
+  "prints nth image nad label from a dataset"
+  [images labels  n]
+  (prn-nth-image-dataset images n)
+  (prn (nth-label labels (dec n))))
+
+(comment
+  (take 10  T10-IMAGES)
+
+  (nth-image T10-IMAGES 1)
+
+  (prn-nth-image-dataset T10-IMAGES 1)
+
+  (nth-label T10-LABELS 1)
+
+  (prn-nth T10-IMAGES T10-LABELS 1)
+
+  (prn-nth T60-IMAGES T60-LABELS 59999)
+
+  (prn-nth T60-IMAGES T60-LABELS 60000)
+
+  (prn-nth T60-IMAGES T60-LABELS 8)
+
+
+  (count (partition image-size-squared T10-IMAGES))
+
+  (count (partition image-size-squared T60-IMAGES))
+
+  (prn-nth T60-IMAGES T60-LABELS 12333)
+  (prn-nth T60-IMAGES T60-LABELS 23233)
+
+  (str (int (/  (count T60-IMAGES) 1000000)) " million")
+
+  (time (prn-nth T60-IMAGES T60-LABELS 23233))
+  
+  (time (prn-nth T60-IMAGES T60-LABELS 60000))
+  
+  
+
+  ;;;
+  )
+
+(defn partition-imgvec
+  "Returns vec of vecs w/ iamges"
+  [images]
+  (->>
+   (partition image-size-squared images)
+   (mapv vec)))
+
+(comment
+  ; k-means
+
+  (def xs-flat (take-images-range  1 500 T60-IMAGES))
+  (def xs-labels (subvec T60-LABELS 0 500))
+
+
+  (count xs-flat)
+  (count xs-labels)
+
+
+  (prn-nth xs-flat xs-labels 999)
+
+  (count (partition image-size-squared xs-flat))
+  (first (partition image-size-squared xs-flat))
+
+  (count (first (partition-imgvec xs-flat)))
+
+  (def xs (partition-imgvec xs-flat))
+  (count xs)
+
+  (def gs (k-means-rand-z xs :k 20 :i 10))
+  (def gs (k-means-rand-x->z xs :k 20 :i 20)) ; split into 20 gs randomly and do 20 iterations
+  (def gs (time (k-means-rand-gs xs :k 20 :i 8)))
+  ; 3 iters 100 sec 9 iter 186 sec - because incorrect validation for = J J-prev  20i 420sec
+
+  (gs :iter)
+  (count (gs :groups))
+
+  (doseq [z (gs :zs)]
+    (prn-image (map int z)))
+
+  (prn-image (first xs))
+  (prn-image (first (gs :zs)))
+  (prn-image (nth (gs :zs) 6))
+  (prn-image (nth (gs :zs) 7))
+
+  (def t (time ((fn [] (prn 'hello) 3))))
+
+  (defn long-running-job [n]
+    (Thread/sleep 3000) ; wait for 3 seconds
+    (+ n 10))
+
+  (time (doall (map long-running-job (range 4))))
+  (time (doall (pmap long-running-job (range 4))))
+  
+  ;;;
+  )
