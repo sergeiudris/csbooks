4228c8c26d11fea7356924eac44fd202cb502edb
sergeiudris <sergei.udris@gmail.com>
2019-05-19 14:29:19 +0300

    math tests

--- applied-la/lab/test/ala/math_test.clj
+++ applied-la/lab/test/ala/math_test.clj
@@ -3,14 +3,17 @@
             [ala.math :refer :all]))
 
 (deftest rms-test
-  (testing "root-mean-squared squared equals sum of mean squared and stadndard deviation squared"
-    (let [a [1 -2 3 2]]
-      (is (==* (with-precision 1 (sq (root-mean-square a))) (+ (sq (vec-avg a)) (sq (vec-standard-deviation a)))))))
-  (testing "standardized vec has mean 0"
-    (is (== (vec-avg (vec-standardized a)) 0)))
-  (testing "standardized vec has standard deviation 1"
-    (is (== (vec-standard-deviation (vec-standardized a)) 1)))
-  
+  (let [a [1 -2 3 2]]
+    (testing "root-mean-squared squared equals sum of mean squared and stadndard deviation squared"
+      (is (==*
+           (with-precision 1 (sq (root-mean-square a)))
+           (+ (sq (vec-mean a))
+              (sq (vec-standard-deviation a))))))
+
+    (testing "standardized vec has mean 0"
+      (is (== (vec-mean (vec-standardized a)) 0)))
+    (testing "standardized vec has standard deviation 1"
+      (is (== (vec-standard-deviation (vec-standardized a)) 1))))
   ;
   )
 
@@ -18,9 +21,22 @@
   (is (== (rad->deg (vec-angle [1 0 0] [0 0 1])) 90))
   )
 
+(deftest mx-test
+  (testing "transpose of sum is th esum of transposes")
+  (let [A (rows->mx [[1 2 3]
+                     [2 1 4]
+                     [3 4 1]])
+        B (rows->mx [[3 1 0]
+                     [2 1 4]
+                     [-1 9 3]])]
+    (is (=   (mx-transpose 3 (elwise-sum A B))
+             (elwise-sum (mx-transpose 3 A) (mx-transpose 3 B)))))
+  
+  )
 
 
 (comment
   (with-precision 1 (sq (root-mean-square [1 -2 3 2])))
+  (run-tests)
   ;;;
   )
