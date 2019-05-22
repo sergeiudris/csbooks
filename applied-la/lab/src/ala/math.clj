a8118f1f33352576b6e96d0d5e1ca085ddd950c1
sergeiudris <sergei.udris@gmail.com>
2019-05-22 07:37:47 +0300

    QR-factorization

--- applied-la/lab/src/ala/math.clj
+++ applied-la/lab/src/ala/math.clj
@@ -1356,16 +1356,53 @@
   "Returns an orthonormal collection of vectors qi..qk.
    Each xi is a linear comb of qs and each qi is a lincomb of xi..xk "
   ([xs]
-   (gram-schmidt-qs xs []))
-  ([xs qs]
-   (if (empty? xs) qs
+   (gram-schmidt-qs xs [] []))
+  ([xs qs- qs]
+   (if (empty? xs) [qs- qs]
        (let [xi (first xs)
              qi (gram-schmidt-q xi qs)]
          (cond
-           (every? #(==* 0 %) qi) (cprn qi)
-           :else (recur (vec (rest xs)) (vec (conj qs (vec-normalize qi))))
+           (every? #(==* 0 %) qi) (do (cprn qi) false)
+           :else (recur (vec (rest xs))  (vec (conj qs- qi)) (vec (conj qs (vec-normalize qi))) )
          ;
            )))))
+
+(defn mx->qs
+  "Returns the Q mx with cols as orthonormal vecs prodcued by applying Gram-Schmidt
+   to A's cols.
+   "
+  [wid A]
+  (as-> nil E
+    (mx->cols wid A)
+    (gram-schmidt-qs E)))
+
+(defn QR-factorization->Q
+  "Returns Q mx formed by Gram-Schmidt alg"
+  [wid A]
+  (as-> nil E
+    (mx->cols wid A)
+    (second (gram-schmidt-qs E))
+    (cols->mx  E)))
+
+(defn QR-factorization->R
+  "Returns the R triangular mx for A = QR"
+  [widA A]
+  (let [widR     widA
+        R        (make-mx widR widR nil)
+        xs       (mx->cols widA A)
+        [qs- qs] (mx->qs widA A)]
+    (->
+     (map-indexed (fn [i _]
+                    (let [i-row (index->row i widR)
+                          i-col (index->col i widR)
+                          qi    (qs i-row)
+                          xj    (xs i-col)
+                          qi-   (qs- i-row)]
+                      (cond
+                        (= i-row i-col) (vec-norm qi-)
+                        (< i-row i-col)  (inner-prod qi xj)
+                        (> i-row i-col) 0))) R)
+     vec)))
 
 
 (comment
@@ -1373,25 +1410,51 @@
   (gram-schmidt-qs [[2 1] [1  0] [0 1]])
   (gram-schmidt-qs [[1 1] [-3 2] [2 4]]) ; should be dependent
   (gram-schmidt-qs [[1 4 2 -3] [7 10 -4 -1] [-2 1 5 -4]])
-  
+
 
 
   (def a1 [-1 1 -1 1])
   (def a2 [-1 3 -1 3])
   (def a3 [1 3 5 7])
 
-  (def qs (gram-schmidt-qs [a1 a2 a3]))
+  (def A (cols->mx [a1 a2 a3]))
+  (cprn-mx 3 A)
+  (def qs (second (gram-schmidt-qs [a1 a2 a3])))
   (def Q (cols->mx  qs))
   (cprn-mx 3 Q)
-  (cprn-mx 3 (mx-prod 4 3 (mx-transpose 3 Q) Q)) ; should be Identity and it is!
+  (cprn-mx 3 (mx-prod 4 3 (mx-transpose 3 Q) Q)) ; should be Identity and it is
+
+  (mx->cols 3 A)
+  (gram-schmidt-qs (mx->cols 3 A))
+  (cprn-mx 3 (cols->mx (mx->qs 3 A)))
+
+  
+  (def R (QR-factorization->R 3 A))
+  (cprn-mx 3 R)
+  (cprn-mx 3 (mx-prod 3 3 Q R))
+  (=* A (mx-prod 3 3 Q R))
+  
+  (=* A (mx-prod 3 3 Q R))
+  
+  (cprn-mx 3 (QR-factorization->Q 3 A) )
+  
+  (=* A (mx-prod 3 3 (QR-factorization->Q 3 A) (QR-factorization->R 3 A)) )
+  
+  
+  (cprn-mx 3 (mx-prod 3 3 Q (rows->mx [[2 4 2]
+                                       [0 2 8]
+                                       [0 0 4]])))
 
   (def a [1 1 1])
   (def b [1 2 0])
   (def c [0 -1 1])
 
+  (vec-norm [-0.5 0.5 -0.5 0.5])
+  
   (gram-schmidt-qs [a b c])
 
   (every? zero? [0.0 0.0 0.0 -0.0])
+
 
 
   ;;;
