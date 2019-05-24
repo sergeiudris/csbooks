1f66a88b1001ae563208bb0ace9b27d738ea1d5c
sergeiudris <sergei.udris@gmail.com>
2019-05-24 09:28:38 +0300

    iris regression model

---
+++ applied-la/lab/src/ala/iris.clj
@@ -0,0 +1,97 @@
+(ns ala.iris
+  (:require [clojure.repl :refer :all]
+            [ala.print :refer [cprn]]
+            [ala.math :refer :all]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io])
+  ;
+  )
+
+(defn read-column [filename column-index]
+  (with-open [reader (io/reader filename)]
+    (let [data (csv/read-csv reader)]
+      ; (map #(nth % column-index) data) ; lazy
+      (mapv #(nth % column-index) data))))
+
+(defn read-csv-file
+  [filename]
+  (with-open [reader (io/reader filename)]
+    (let [data (doall (csv/read-csv reader))]
+      data)))
+
+
+(def iris-filename "data/iris/iris.data")
+
+(defn try-str->num
+  "returns number or nil"
+  [strin]
+  (try (Float/parseFloat strin) (catch Exception e strin)))
+
+(defn parse-numbers
+  [data]
+  (mapv #(mapv try-str->num %) data))
+
+(defn read-csv-data
+  "reads and parses csv data"
+  [filename]
+  (-> (read-csv-file filename)
+      parse-numbers))
+
+(def IRIS_DATA_RAW (read-csv-file iris-filename))
+(def IRIS_DATA (->  IRIS_DATA_RAW parse-numbers drop-last vec))
+
+(count IRIS_DATA)
+
+
+
+(comment
+  (sign 0)
+
+  ;162 Regression model
+  ;299 iris
+
+  (def samples (mapv #(vec (drop-last  %)) IRIS_DATA))
+  (def labels (mapv #(last %)  IRIS_DATA))
+  (def binary-lables-virginica (mapv #(if (= % "Iris-virginica") 1 -1) labels))
+  (def binary-lables-setosa (mapv #(if (= % "Iris-setosa") 1 -1) labels))
+  (def binary-lables-versicolor (mapv #(if (= % "Iris-versicolor") 1 -1) labels))
+
+
+
+
+
+
+  (count samples)
+  (def X (make-regression-model-feature-mx samples))
+  (count X)
+  (def heiXT (/ (count X) 5))
+  (take 5 X)
+
+  (def params (regression-model-parameters 150 X binary-lables-virginica))
+  ; values are not precisly correct v = −2.39, β 1 = −0.0918, β 2 = 0.406,β 3 = 0.00798,β 4 = 1.10.
+  ; [-2.370893168639163
+  ;  -0.08819672913977447
+  ;  0.3964650446833074
+  ;  0.008480010289205383
+  ;  1.0930853325898924]
+
+  (def v (first params))
+  (def bs (vec (rest params)))
+
+  (def y- (regression-model-predictions 150 X params))
+
+  (def y-- (mapv sign  y-))
+  (count (filterv pos? y--))
+  (- 1 (float (/ 50 53)))
+
+  (def residuals (elwise-subtract binary-lables-virginica y--))
+
+  (root-mean-square residuals)
+  (def relative-pred-error (/ (root-mean-square residuals) (root-mean-square binary-lables-virginica)))
+
+  (root-mean-square [-1 -1 1])
+
+  (float (error-rate binary-lables-virginica y--)) ; 0.07333333 correct
+
+  ;;;
+  )
--- applied-la/lab/src/ala/least_squares.clj
+++ applied-la/lab/src/ala/least_squares.clj
@@ -6,7 +6,7 @@
   )
 
 
-(defn least-squares-approximate
+(defn least-squares-x
   "Returns the approximate solution to 
   Ax=b
   "
@@ -34,11 +34,11 @@
 
   (QR-linear-equation 2 A [1 0 -1])
 
-  (least-squares-approximate 2 A [1 0 -1])
+  (least-squares-x 2 A [1 0 -1])
 
   (linear-equation-pseudo-inverse 2 A [1 0 -1])
 
-  (least-squares-approximate 2 A [2 1 0])
+  (least-squares-x 2 A [2 1 0])
 
 
   (def B (cols->mx [[1 0 -1]
@@ -49,10 +49,60 @@
   (def B (cols->mx [[1 0 -1]
                     [2 1 0]
                     [-1 3 1]]))
-  (least-squares-approximate 2 A [-1 3 1])
+  (least-squares-x 2 A [-1 3 1])
   
 
   (prn-mx 3 (least-squares-X 2 3 A B))
+
+  ;;;
+  )
+
+
+(comment
+
+  (def R (rows->mx [[0.97 1.86 0.41]
+                    [1.23 2.18 0.53]
+                    [0.80 1.24 0.62]
+                    [1.29 0.98 0.51]
+                    [1.10 1.23 0.69]
+                    [0.67 0.34 0.54]
+                    [0.87 0.26 0.62]
+                    [1.10 0.16 0.48]
+                    [1.92 0.22 0.71]
+                    [1.29 0.12 0.62]]))
+
+  (/ 2.18 0.12)
+
+  (def v-des (make-vec 10 1000))
+
+  (least-squares-x 3 R v-des)
+  ; [62.07662454385152 99.98500402826195 1442.837462541221]
+
+  (Math/round 62.07662454385152)
+
+  ; (/ 2M 3M)
+  ; (with-precision 2 (/ 2M 3M))
+
+  (root-mean-square (least-squares-x 3 R v-des))
+  
+  (hash "asd")
+  
+  (.hashCode "asd")
+  
+  ;;;
+  )
+
+
+
+
+(comment
+
+;   the n-vector x (i) is the feature vector and the scalar y (i) is the associated
+; value of the outcome for data sample i. We sometimes refer to the pair x (i) , y (i)
+; as the ith data pair. These data are also called observations, examples, samples
+; or measurements, depending on the context.
+
+  
 
   ;;;
   )
