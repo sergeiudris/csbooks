4cb29679571adf5cf260efaa42fc096dcc5977df
sergeiudris <sergei.udris@gmail.com>
2019-08-17 13:05:43 +0300

    struc

--- sicp/app/project.clj
+++ sicp/app/project.clj
@@ -11,12 +11,12 @@
                  ]
   :min-lein-version "2.8.3"
   :resource-paths ["config", "resources"]
-  :repl-options {:init-ns core
-                 :main core
+  :repl-options {:init-ns main
+                 :main main
                 ;  :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                  }
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "core/-main"]}
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "main/-main"]}
                    :dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]]}}
-  :main ^{:skip-aot true} core
+  :main ^{:skip-aot true} main
   
   )
